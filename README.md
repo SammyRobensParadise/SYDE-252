d3996189a4f454a986b2ef58440e68efc2d60543
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-09-24 18:12:54 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SYDE-252
+
+## Assignment 2
+### Required Task:
+plot imaginary part of signal x(t) = e^[-2(1+j*pi)*t]
+### Files
+1. `assignment_2.m`
--- assignment_2.m
+++ assignment_2.m
@@ -1,11 +1,6 @@
-%% Plot a sine signal
-% t=0:0.1:10 % forming a time access from 0 to 10 with 0.1 time steps
-% x=sin(t);
-% x(0) %Uncomment this line and execute it. Why does it cause an error?
-% plot(t,x)
 
+% Sammy Robens-Paradise
 % plot imaginary part of signal x(t) = e^[-2(1+j*pi)*t]
-% define imaginary number
 x=exp(-2*(1+1i*pi)*t);
 im = imag(x);
 plot(im);
